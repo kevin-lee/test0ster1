31ec4ac65bb5efe8ec4f73680aedb3bb9e303553
Kevin Lee <kevin.code@kevinlee.io>
2019-11-21 16:18:16 +1100

    Close #6 - Change group ID to io.kevinlee

---
+++ project/ProjectInfo.scala
@@ -0,0 +1,19 @@
+import sbt.{Developer, ScmInfo, url}
+
+object ProjectInfo {
+  val ProjectVersion: String = "0.0.7"
+
+  val projectDevelopers: List[Developer] = List(
+    Developer(
+        "Kevin-Lee", "Kevin Lee"
+      , "kevin.code@kevinlee.io"
+      , url("https://github.com/Kevin-Lee")
+    ))
+
+  val projectHomePage: Option[sbt.URL] = Some(url("https://github.com/Kevin-Lee/test0ster1"))
+
+  val projectScmInfo: Option[ScmInfo] = Some(ScmInfo(
+        url("https://github.com/Kevin-Lee/test0ster1")
+      , "git@github.com:Kevin-Lee/test0ster1.git"
+    ))
+}
